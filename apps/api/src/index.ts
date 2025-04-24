21e507909626da6eb22d163ab533b976fd414d22
Renato Marinho <renato.marinhofr@gmail.com>
2025-04-24 14:48:41 -0300

    feat(auth): enhance permissions and role management for projects and billing

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,8 @@
-import { defineAbilityFor } from "@saas/auth";
+import { defineAbilityFor, projectSchema } from "@saas/auth";
 
-const ability = defineAbilityFor({ role: "MEMBER" });
-const userCanDeleteOtherUsers = ability.can("delete", "User");
+const ability = defineAbilityFor({ role: "MEMBER", id: "user-id" });
+const project = projectSchema.parse({ id: "project-id", ownerId: "user-id" });
 
-const userCannotDeleteOtherUsers = ability.cannot("delete", "User");
-
-console.log(userCannotDeleteOtherUsers);
-console.log(userCanDeleteOtherUsers);
+console.log(ability.can("get", "Billing"));
+console.log(ability.can("create", "Invite"));
+console.log(ability.can("delete", project));
