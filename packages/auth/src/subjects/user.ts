fe6bf9a7b8a9d8bfcbcee09f53f8944ac1ca7a31
Renato Marinho <renato.marinhofr@gmail.com>
2025-04-24 14:36:40 -0300

    feat(auth): enhance permission management with project and organization schemas

--- packages/auth/src/subjects/user.ts
+++ packages/auth/src/subjects/user.ts
@@ -3,14 +3,10 @@ import { z } from "zod";
 export const userSubject = z.tuple([
 	z.union([
 		z.literal("manage"),
-
 		z.literal("get"),
-
 		z.literal("update"),
-
 		z.literal("delete"),
 	]),
-
 	z.literal("User"),
 ]);
 
