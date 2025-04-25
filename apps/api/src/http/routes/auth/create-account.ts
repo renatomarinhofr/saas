cd8d12132016827742d75882e65607f4bff9649a
Renato Marinho <renato.marinhofr@gmail.com>
2025-04-25 19:31:23 -0300

    feat(auth): implement user account creation endpoint with Zod validation

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+
+export async function createAccount(app: FastifyInstance) {
+	app.withTypeProvider<ZodTypeProvider>().post(
+		"/users",
+		{
+			schema: {
+				body: z.object({
+					name: z.string(),
+					email: z.string().email(),
+					password: z.string().min(6),
+				}),
+			},
+		},
+		() => {
+			return "User created";
+		},
+	);
+}
